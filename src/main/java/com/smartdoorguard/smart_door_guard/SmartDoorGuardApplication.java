c30a1f1178330c0c0fd19ebe4060fe9588e6e9aa
sopp1313 <dldpdms0131@gmail.com>
2026-07-31 19:00:52 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/smartdoorguard/smart_door_guard/SmartDoorGuardApplication.java
@@ -0,0 +1,13 @@
+package com.smartdoorguard.smart_door_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDoorGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDoorGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartdoorguard/smart_door_guard/SmartDoorGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdoorguard.smart_door_guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDoorGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
